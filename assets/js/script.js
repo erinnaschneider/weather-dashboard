b5774894badb9b1a0dd1fc1e7adb454271e6fe6b
Erinn Schneider <erinnaschneider@gmail.com>
2021-11-22 17:16:30 -0500

    tutoring session help

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,23 @@
 // variables
 let apiKey = "0b71a56e08e923ace97d75b3cf84952f";
-let searchInputs = document.getElementById("searchInput");
+let searchHistory = document.getElementById("searchInput");
 let searchedValue = document.getElementById("searched");
 let searchButton = document.getElementById("button");
 let searchedCities = [];
 let forecastDiv = document.getElementById("5dayforecast");
 let allWeather = document.getElementById("weatherInfo");
 
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
 // save user inputs to local storage when search button is clicked
   function saveUserInputs(event) {
     // stop form from submitting
     event.preventDefault();
+    var userinput = searchedValue.value.trim()
     // creating a saved cities object to push into an array for storage
     let inputText = {
-        city: searchedValue.value.trim()
+        city: userinput
     };
 
     searchedCities.push(inputText.city);
@@ -31,8 +35,43 @@ let allWeather = document.getElementById("weatherInfo");
     let cityList = document.createElement("div");
     cityList.innerHTML = "<p>" + inputText.city + "</p>"
        
-    searchedInputsList.append(cityList);
+    //searchedInputsList.append(cityList);
+    getCoords(userinput)
 };
+
+function getCoords(cityname){
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiKey}`).then(function (data){
+        return data.json()
+    }).then(function(res){
+        getWeatherData(res)
+    })
+}
+
+function getWeatherData(coordinates){
+    //console.log(coordinates)
+    var lat = coordinates.coord.lat;
+    var lon =  coordinates.coord.lon;
+    var city = coordinates.name;
+
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,alerts&units=imperial&appid=${apiKey}`).then(function(data){
+return data.json()
+    }).then(function(res){
+    renderCurrentWeather(city, res.current, res.timezone)
+    })
+}
+
+function renderCurrentWeather(city, weather, timezone){
+    var date = dayjs().tz(timezone).format('M/D/YYYY');
+    let temperature = weather.temp;
+    let humidity = weather.humidity;
+    let uvIndex = weather.uvi;
+    let windSpeed = weather.wind_speed;
+    let icon = weather.weather[0].icon;
+    //city date/temp/humid/windspeed/uv
+
+}
+
 
 // add event listeners 
 searchButton.addEventListener("click", saveUserInputs);
